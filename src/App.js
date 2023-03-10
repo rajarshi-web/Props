cf82acfc8a956a054b647ab32c7ab8d45ee5cbcf
rajarshi-web <rajarshimandal19@gmail.com>
2023-03-10 22:14:42 +0530

    Upload all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Profile from './components/Component';
+function App() {
+  return (
+    <div className="App">
+      <Profile text="Hi World"></Profile>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Component.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+ export default function Profile()
+  {
+    return(
+        <div>
+            <h3>Head</h3>
+        </div>
+    )
+  }
